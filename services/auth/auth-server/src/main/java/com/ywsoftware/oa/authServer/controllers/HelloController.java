bbf00be0f1739af467fd62ddb5d7786fe211115c
foolishqi <qiy@ywsoftware.com>
2018-06-01 17:59:13 +0800

    enable auth server config client.

--- services/auth/auth-server/src/main/java/com/ywsoftware/oa/authServer/controllers/HelloController.java
+++ services/auth/auth-server/src/main/java/com/ywsoftware/oa/authServer/controllers/HelloController.java
@@ -2,9 +2,13 @@ package com.ywsoftware.oa.authServer.controllers;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class HelloController {
@@ -15,5 +19,27 @@ public class HelloController {
     @RequestMapping("/hi")
     public ResponseEntity<String> hi(@RequestParam String name) {
         return ResponseEntity.ok("hello world " + name + ". port: " + port);
+    }
+
+    @Value("${config.a}")
+    String a;
+    @Value("${config.b}")
+    String b;
+    @Value("${config.c}")
+    String c;
+    @Value("${config.d}")
+    String d;
+    @Value("${config.e}")
+    String e;
+
+    @GetMapping("/env")
+    public ResponseEntity<List<String>> env() {
+        List<String> config = new ArrayList<String>();
+        config.add(a);
+        config.add(b);
+        config.add(c);
+        config.add(d);
+        config.add(e);
+        return ResponseEntity.ok(config);
     }
 }
