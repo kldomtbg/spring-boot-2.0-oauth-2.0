0daa0ea85ec68959b845855e9190caea2a15f81e
qianyf <f123456>
2018-06-06 11:16:45 +0800

    添加空的资源服务

---
+++ example/auth-resources/src/main/java/com/example/authresources/AuthResourcesApplication.java
@@ -0,0 +1,12 @@
+package com.example.authresources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthResourcesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthResourcesApplication.class, args);
+    }
+}
---
+++ example/auth-resources/src/test/java/com/example/authresources/AuthResourcesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.authresources;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AuthResourcesApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
