38220da9c1e95f1c038ff4fe800ed139e46fa142
fuy <s123456>
2018-06-07 09:09:02 +0800

    解决冲突

---
+++ services/auth/auth-server/src/main/java/com/ywsoftware/oa/authServer/base/jdbc/UserRepositoryImpl.java
@@ -0,0 +1,35 @@
+package com.ywsoftware.oa.authServer.base.jdbc;
+
+import com.ywsoftware.oa.authServer.core.entity.User;
+import com.ywsoftware.oa.authServer.core.repositorys.UserRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepositoryImpl implements UserRepository {
+
+    private UserRepositoryInterface userRepositoryInterface;
+
+    public UserRepositoryImpl(UserRepositoryInterface _userRepositoryInterface) {
+        this.userRepositoryInterface = _userRepositoryInterface;
+    }
+
+    @Override
+    public User read(String id) {
+        return userRepositoryInterface.findUser(id);
+    }
+
+    @Override
+    public User create(User user) {
+        return userRepositoryInterface.save(user);
+    }
+
+    @Override
+    public void update(String name, String id) {
+        userRepositoryInterface.update(name, id);
+    }
+
+    @Override
+    public void delete(String id) {
+        userRepositoryInterface.deleteById(id);
+    }
+}
