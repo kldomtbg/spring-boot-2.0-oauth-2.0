6365442dd5acc01962e05752ba0f58be707bb804
fuy <s123456>
2018-06-07 14:57:08 +0800

    非应用异常

--- services/auth/auth-server/src/main/java/com/ywsoftware/oa/authServer/base/exceptions/GlobalExceptionHandler.java
+++ services/auth/auth-server/src/main/java/com/ywsoftware/oa/authServer/base/exceptions/GlobalExceptionHandler.java
@@ -24,6 +24,7 @@ public class GlobalExceptionHandler {
 
         } else {
             //TODO 非应用异常
+            logger.error(ex.getMessage(), ex);
         }
         return ex.getMessage();
     }
