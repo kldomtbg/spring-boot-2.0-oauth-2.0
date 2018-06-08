561ded2318ac9cbd3d6a06e7a486b7f22635ab17
qianyf <f123456>
2018-06-08 10:02:32 +0800

    /api/**

--- services/auth/auth-server/src/main/java/com/ywsoftware/oa/authserver/controller/ClientController.java
+++ services/auth/auth-server/src/main/java/com/ywsoftware/oa/authserver/controller/ClientController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/client")
+@RequestMapping("/api/client")
 public class ClientController {
 
     private ClientService clientService;
